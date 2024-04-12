193f5e689e65672aa68a290eee9a7d83cad2ea3f
Carlos Perales <carlosperales050@gmail.com>
2024-04-12 10:45:50 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-red-500">Holi</h1>
+    </>
+  );
+}
+
+export default App;
